e8d34348f695d60a53495998325f84aefa0e5b00
rithalyarajput-creator <social.marketing@salarytopup.com>
2026-05-30 13:57:06 +0530

    About Me: rotate second school photo to landscape

--- src/app/sandeep-rajput/SchoolGallery.tsx
+++ src/app/sandeep-rajput/SchoolGallery.tsx
@@ -6,11 +6,13 @@ const PHOTOS = [
     src: 'https://9qidomuaf1nvlbrh.public.blob.vercel-storage.com/uploads/1780126495893-sandeep-rajput-rithalya-rajput-rana-pratap-sarvodaya-school-rithala-delhi.jpg-aOf2OUhgtMt1zncuB5nE4EAvhOsiRU.jpg',
     alt: 'Sandeep Rajput Rithalya Rajput with friends at Rana Pratap Sarvodaya School Rithala Delhi — Birthday Celebration',
     caption: 'Birthday celebration with classmates at Rana Pratap School, Rithala',
+    rotate: false,
   },
   {
     src: 'https://9qidomuaf1nvlbrh.public.blob.vercel-storage.com/uploads/1780128070439-Sandeep_Rajput_at_Rana_Pratap_Sarvodaya_School_Rithala_Delhi-HiluKN1YF4KDt9BXsd0w7AQkxNocxX.webp',
     alt: 'Sandeep Rajput Rithalya Rajput at Rana Pratap Sarvodaya Bal Vidyalaya School Rithala Delhi with school friends',
     caption: 'With friends at Rana Pratap School, Rithala, Delhi',
+    rotate: true,
   },
 ];
 
@@ -35,6 +37,7 @@ export default function SchoolGallery() {
                 src={PHOTOS[active].src}
                 alt={PHOTOS[active].alt}
                 className="sr-sg-main-img"
+                style={PHOTOS[active].rotate ? { transform: 'rotate(90deg)', maxHeight: '55vw', width: 'auto', margin: '0 auto' } : {}}
               />
               <p className="sr-sg-caption">{PHOTOS[active].caption}</p>
             </div>
